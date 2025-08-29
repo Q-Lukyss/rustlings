b769c5805dabf25e95413a8566c84dfc65771e04
Q-Lukyss <quentin.lkss@gmail.com>
2025-08-29 10:01:49 +0200

    first 10 exo clear

---
+++ exercises/08_enums/enums2.rs
@@ -0,0 +1,38 @@
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+#[derive(Debug)]
+enum Message {
+    // TODO: Define the different variants used below.
+    Resize { width: u64, height: u64 }, // struct-like
+    Move(Point),                        // tuple-like
+    Echo(String),                       // tuple-like
+    ChangeColor(u8, u8, u8),            // tuple-like (RGB en u8)
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{self:?}");
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Resize {
+            width: 10,
+            height: 30,
+        },
+        Message::Move(Point { x: 10, y: 15 }),
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
